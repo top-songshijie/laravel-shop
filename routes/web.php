48ebb21e65df029f5a642b950e5264d32d28e9dc
songshijie <997031758@qq.com>
2019-07-31 18:19:50 +0800

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,10 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
 
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 
 });
 
