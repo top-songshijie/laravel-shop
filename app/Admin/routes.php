030b197ed099a570ff948b76fa609902de9fd208
songshijie <997031758@qq.com>
2019-08-05 07:15:25 +0000

    管理后台 - 拒绝退款

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -25,4 +25,6 @@ Route::group([
 
     $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
 
+    $router->post('orders/{order}/refund', 'OrdersController@handleRefund')->name('admin.orders.handle_refund');
+
 });
