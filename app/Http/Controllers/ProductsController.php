f41d4f63781cf4ece3e1f32d8ef4b6a37a8c8de8
vagrant <vagrant@homestead>
2019-08-09 16:24:02 +0000

    前台商品列表支持按类目筛选

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -65,6 +65,7 @@ class ProductsController extends Controller
                 'search' => $search,
                 'order'  => $order,
             ],
+            'category' => $category ?? null,
         ]);
     }
 
